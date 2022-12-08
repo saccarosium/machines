7dfcf85326672de1008b09e8ffd5af9c96e6ad2f
saccarosium <github.e41mv@aleeas.com>
2022-12-08 12:09:00 +0100

    Refactor darwin config

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -1,64 +1,109 @@
 { pkgs, ... }:
+let
+  home = builtins.getEnv "HOME";
+in
 {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [
-    bashInteractive
-    bash-completion
-    nix-bash-completions
-  ];
+  environment = {
+    variables = {
+      LANG = "en_US.UTF-8";
+    };
 
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-  programs.bash.enable = true;
-  programs.bash.enableCompletion = true;
+    darwinConfig = "${home}/Repos/machines/modules/darwin.nix";
+    systemPackages = with pkgs; [
+      bashInteractive
+      bash-completion
+      nix-bash-completions
+    ];
+  };
+
   nix.package = pkgs.nix;
-  environment.darwinConfig = "/Users/sacca/Repos/machines/modules/darwin.nix";
+  services = {
+    nix-daemon.enable = true;
+  };
 
-  system.defaults.NSGlobalDomain.KeyRepeat = 8;
-  system.defaults.NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
-  system.defaults.NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
-  system.defaults.NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
-  system.defaults.NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
+  programs = {
+    bash = {
+      enable = true;
+      enableCompletion = true;
+    };
+  };
 
-  system.defaults.SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+  system = {
+    defaults = {
+      NSGlobalDomain = {
+        KeyRepeat = 2;
+        AppleInterfaceStyle = "Dark";
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSDocumentSaveNewDocumentsToCloud = false;
+      };
+      dock = {
+        autohide = true;
+        autohide-delay = 0.0;
+        autohide-time-modifier = 0.0;
+        expose-animation-duration = 0.0;
+        orientation = "left";
+        wvous-bl-corner = 1;
+        wvous-br-corner = 1;
+        wvous-tl-corner = 1;
+        wvous-tr-corner = 1;
+      };
+      SoftwareUpdate = {
+        AutomaticallyInstallMacOSUpdates = true;
+      };
+      alf = {
+        stealthenabled = 1;
+      };
+      loginwindow = {
+        GuestEnabled = false;
+      };
+    };
+  };
 
-  system.defaults.alf.stealthenabled = 1;
-
-  system.defaults.dock.autohide = true;
-  system.defaults.dock.autohide-delay = 0.0;
-  system.defaults.dock.autohide-time-modifier = 0.0;
-  system.defaults.dock.expose-animation-duration = 0.0;
-  system.defaults.dock.orientation = "left";
-
-  system.defaults.loginwindow.GuestEnabled = false;
-
-  homebrew.enable = true;
-  homebrew.onActivation.cleanup = "uninstall";
-  homebrew.onActivation.upgrade = true;
-  homebrew.brews = [ "bash" ];
-  homebrew.casks = [
-    "anki"
-    "brave-browser"
-    "discord"
-    "ferdium"
-    "hammerspoon"
-    "karabiner-elements"
-    "keepassxc"
-    "keepingyouawake"
-    "little-snitch"
-    "logisim-evolution"
-    "mactex-no-gui"
-    "nextcloud"
-    "onlyoffice"
-    "onyx"
-    "pika"
-    "protonvpn"
-    "raspberry-pi-imager"
-    "signal"
-    "tor-browser"
-    "via"
-    "visual-studio-code"
-    "wezterm"
-  ];
+  homebrew = {
+    enable = true;
+    onActivation = {
+      cleanup = "zap";
+      upgrade = true;
+    };
+    taps = [
+      "homebrew/bundle"
+      "homebrew/cask"
+      "homebrew/core"
+      "homebrew/services"
+      "wez/wezterm"
+    ];
+    brews = [
+      "bash"
+    ];
+    casks = [
+      "anki"
+      "brave-browser"
+      "discord"
+      "ferdium"
+      "hammerspoon"
+      "karabiner-elements"
+      "keepassxc"
+      "keepingyouawake"
+      "little-snitch"
+      "logisim-evolution"
+      "mactex-no-gui"
+      "nextcloud"
+      "onlyoffice"
+      "onyx"
+      "pika"
+      "protonvpn"
+      "raspberry-pi-imager"
+      "signal"
+      "tor-browser"
+      "via"
+      "visual-studio-code"
+      "wezterm"
+    ];
+  };
 }
